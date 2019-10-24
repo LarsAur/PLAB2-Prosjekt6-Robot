a4ee8bbb4ff290da17f1e78240ec8c535bb44295
LarsAur <lars1998@hotmail.com>
2019-10-24 15:19:03 +0200

    added wrapper files to project6_supply

--- motob.py
+++
@@ -1,6 +0,0 @@
-
-class Motob():
-
-def __init__(self, motors):
-    self.motors
-    self.value = 0
---
+++ project6_supply/motob.py
@@ -0,0 +1,16 @@
+
+class Motob():
+
+
+    def __init__(self, motors):
+        self.motors = motors
+        self.value = 0
+
+
+    def update(self, motor_recomendations):
+        self.value = motor_recomendations
+        self.operationalize()
+
+
+    def operationalize(self):
+        for 
