e8a128042f957ec6607f102ba4dcb860676e74db
LarsAur <lars1998@hotmail.com>
2019-10-24 13:09:52 +0200

    add bbco methods without implementations

---
+++ BBCO.py
@@ -0,0 +1,20 @@
+class bbco():
+
+    def __init__():
+        self.behaviors = []
+        self.active_bahaviors = []
+        self.sensobs = []
+        self.motobs = []
+        self.arbitor = Arbitor()
+
+    def add_behavior(behavior):
+        pass
+
+    def add_sensob(sensob):
+        pass
+
+    def activate_behavior(behavior):
+        pass
+
+    def deactivate_behavior(behavior):
+        pass
